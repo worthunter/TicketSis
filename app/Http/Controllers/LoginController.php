58503bee4342ae18aeef2e1f8511945f5f18fa93
worthunter <josecast22@outlook.com>
2023-12-14 11:47:12 -0600

    Backup before login configuration

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,64 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Login;
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use \App\Models\User;
 
 class LoginController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        #return view('login.index');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+	
 }
--- resources/views/Login/index.blade.php
+++ resources/views/Login/index.blade.php
@@ -10,27 +10,36 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 
-
     <title>TicketSis · Login</title>
 </head>
 <body style="background-color:rgb(237, 242, 249, 0.96)">
-    <div>
-        <div class="logo img">
-            <img src="/TicketSis/public/img/logoPDV.png"/>
-        </div>
+    @if (session('success'))
+        <h1>{{session('success')}}</h1>
+    @endif
+    <div class="logo img">
+        <img src="/TicketSis/public/img/logoPDV.png"/>
+    </div>
+        <form method="POST" action="ruta login">
+            @csrf
             <div class="loginbox">
                 <p style="font-size: 1.1rem">Introduce e-mail y contraseña</p>
                 <div class="input-group mb-2">
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="basic-addon1"><i class="bi bi-person-bounding-box"></i></span>
                     </div>
-                    <input type="text" class="form-control" placeholder="E-mail" aria-describedby="basic-addon1">
+                    <input type="text" class="form-control" placeholder="E-mail" id="correo" name="correo" required autofocus>
+                    @if ($errors->has('correo'))
+                        <span class="text-danger">{{ $errors->first('email') }}</span>
+                    @endif
                 </div>
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="basic-addon1"><i class="bi bi-shield-lock"></i></span>
                     </div>
-                    <input type="text" class="form-control" placeholder="Contraseña" aria-describedby="basic-addon1">
+                    <input type="password" class="form-control" placeholder="Contraseña" id="contra" name="contra" required>
+                    @if ($errors->has('contra'))
+                        <span class="text-danger">{{ $errors->first('contra') }}</span>
+                    @endif
                 </div>
                 <div class="form-actions clearfix">
                     <div class="pull-right">
@@ -39,6 +48,7 @@
                     <input type="submit" class="btn btn-primary btn-rounded btn-block" value="Ingresar" />
                 </div>
             </div>
-        </div>
+        </form>
+    </div>
 </body>
 </html>
